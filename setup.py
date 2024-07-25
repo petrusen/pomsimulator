8948d5e64f051dadc262cf993216de2d878de692
Enric <enricpz@icloud.com>
2024-07-25 02:46:01 +0200

    Cleaning setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup,find_packages
 
-with open(path.join(here, "README.rst"), encoding="utf-8") as readme_file:
-        readme = readme_file.read()
-
 setup(name='pomsimulator',
       version='1.0',
       author="Enric Petrus, Jordi Buils. Diego Garay-Ruiz",
       author_email="enricpz@icloud.com, jbuils@iciq.es, dgaray@iciq.es",
       description="Simulate the aqueous speciation of polyoxometalates (POMs) from quantum mechanical results",
       packages=find_packages(),
-      long_description=readme,
       install_requires=['numpy>=1.17.3','matplotlib>=3.1.2','networkx>=2.4','scipy>=1.6.1','pandas>=1.5',
                         'scikit-learn>=1.3.2','seaborn']
       )
